8e58a8ed38fbbb86ad37811f693a4a881c84e00e
Nicolas Flores <nicolasjulioflores@gmail.com>
2020-02-18 21:53:15 -0500

    updated datastructures

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # calculus-reasoner
+
+So far, we've decided to stick to the datastructures / approach detailed in the lectures and in the
+book. 
--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,11 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+    {- Datatypes -}
+newtype Expr = Compose [Atom] deriving (Show, Eq)
+data Atom = Var String 
+          | Con String [Expr] deriving (Show, Eq)
+
+data Law = Law String Equation
+type Equation = (Expr, Expr)
