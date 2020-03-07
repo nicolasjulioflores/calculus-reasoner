bdbec9b9dc5e60bb4755e33a1c90e3c4412c9360
JalenLuWang <33009638+JalenLuWang@users.noreply.github.com>
2020-03-06 22:55:51 -0500

    Add files via upload

--- app/Main.hs
+++ app/Main.hs
@@ -5,12 +5,19 @@ import Calculations
 import Parsing
 import Text.Megaparsec
 
+
+-- Takes the user input, parses the input, then it calculates the derivative 
+
+-- Runs the main
+-- Asks for user input 
 main :: IO ()
 main = do putStrLn "Enter expression: " 
           l <- getLine
           _ <- eval l
           return ()
 
+-- parses the string and calculates the derivative
+-- if it cannot parse, it will return "Parse error"
 eval :: String -> IO ()
 eval s = case parseMaybe expr s of
             Just e -> print (calculate claws e)
